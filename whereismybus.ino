cf60427f151ecc19c8b8da0949986439f9d8cd94
aoiob4305 <aoiob4305@gmail.com>
2022-01-08 10:33:06 +0900

    Update whereismybus.ino

--- whereismybus.ino
+++ whereismybus.ino
@@ -2,7 +2,7 @@
 // 현재 오고있는 버스의 남은시간을 가져와 LED에 나타냄
 // 사용모듈: NODEMCU 1.0 (ESP12-e)
 // LED타입: WS2801 RBG 스트립
-// 각줄의 첫번째 LED는 항상켜져 있도록 하고 시간을 1분 간격으로 위치로 
+// 각줄의 첫번째 LED는 항상켜져 있도록 하고 시간을 LED하나당 1분으로 위치처럼 표시 
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
